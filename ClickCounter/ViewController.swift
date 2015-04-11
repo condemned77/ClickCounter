1b59fcbe26a02a1b46ac3bb4a88b87bce9d6265f
mjaeger <huNt@h.home>
2015-04-11 14:56:46 +0200

    ViewController has now a model variable called click_counter.
    The property click_counter corresponds to the model, while the
    label, count_label is just the representation of the model.

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var click_counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         var count_label = UILabel()
         count_label.frame = CGRectMake(150, 150, 60, 60)
-        count_label.text = "0"
+        count_label.text = String(self.click_counter)
         self.view.addSubview(count_label)
     }
 
